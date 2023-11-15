e7d3297b475a1dc71fe087ec7b45fd0d2a39a97a
Barış Özgen <barisozgen@hotmail.com>
2023-11-15 12:40:24 +0300

    Update dockerfiles and YARP gateway policies

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -21,9 +21,25 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         // Setting the claim type for the user's name.
         options.TokenValidationParameters.NameClaimType = "username";
     });
+// Now, we've not needed to do this so far because we're not our client browser is not making a connection to any of our API servers on the back end.
+// our browsers are going to need to make a connection to our SignalR servers and that means it's a cross origin request 
+// and that means we need to send back a header so that our browser allows that request to complete.
+builder.Services.AddCors(options => 
+{
+    //we added in appSettings.json
+    options.AddPolicy("customBarisPolicy", b => 
+    {
+        //and also we added ClientApp in appSetting.json "ClientApp": "http://localhost:3000"
+        //because NextJS app running on this url
+        b.AllowAnyHeader()
+            .AllowAnyMethod().AllowCredentials().WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
 
 // Building the web application.
 var app = builder.Build();
+//now our app will be able to see our cors policies
+app.UseCors();
 
 // Mapping the Reverse Proxy middleware.
 app.MapReverseProxy();
